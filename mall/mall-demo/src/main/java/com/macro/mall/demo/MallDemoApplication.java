f680282aedc7c126fee74b0c6819fcd90fa95683
JS Chae <js.chae1187@hncorp.world>
2026-04-10 13:57:57 +0900

    feat: add mall server startup console message

--- mall/mall-demo/src/main/java/com/macro/mall/demo/MallDemoApplication.java
+++ mall/mall-demo/src/main/java/com/macro/mall/demo/MallDemoApplication.java
@@ -4,8 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MallDemoApplication{
-	public static void main(String[] args) {
-		SpringApplication.run(MallDemoApplication.class, args);
-	}
+public class MallDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MallDemoApplication.class, args);
+        System.out.println("서버가 실행됩니다.");
+    }
 }
